142b38c8a2a3de5a269819c94023fe575b2255b4
madelmund <mat.adelmund@gmail.com>
2019-10-12 14:52:39 -0700

    adding base project with demo of player switching and movement

---
+++ TopDownGameCollab/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+    public float rotateSpeed = 2.0f;
+    public float moveSpeed = 5.0f;
+    Rigidbody2D rb;
+    float startSpeed;
+    float startRotSpeed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startSpeed = moveSpeed;
+        startRotSpeed = rotateSpeed;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0, 0, -x * rotateSpeed));
+        GetComponent<Rigidbody2D>().velocity = y * transform.up * moveSpeed;
+
+    }
+    public void EnableMovement()
+    {
+        rotateSpeed = startRotSpeed;
+        moveSpeed = startSpeed;
+    }
+    public void DisableMovement()
+    {
+        rotateSpeed = 0;
+        moveSpeed = 0;
+    }
+}
---
+++ TopDownGameCollab/Assets/Scripts/PlayerSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitch : MonoBehaviour
+{
+    public List<GameObject> players;
+    public int playerIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = new List<GameObject>();
+        GameObject[] playerArray = GameObject.FindGameObjectsWithTag("Player");
+        for(int i = 0; i < playerArray.Length; i++)
+        {
+            players.Add(playerArray[i]);
+            if(i != 0)
+            {
+                players[i].GetComponent<PlayerMove>().DisableMovement();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            players[playerIndex].GetComponent<PlayerMove>().DisableMovement();
+            playerIndex++;
+            playerIndex %= players.Count;
+            players[playerIndex].GetComponent<PlayerMove>().EnableMovement();
+        }
+        Vector3 pos = players[playerIndex].transform.position;
+        pos.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, pos, 3.0f);
+    }
+}
