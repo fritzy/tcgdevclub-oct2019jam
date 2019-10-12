1461691782b10f0486b8b64f025d51eeb99e75b6
CyberdineStudios <45576180+CyberdineStudios@users.noreply.github.com>
2019-10-12 16:19:14 -0700

    Waypoint Script Added

---
+++ TopDownGameCollab/Assets/Scripts/WaypointSystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointSystem : MonoBehaviour
+{
+    [Tooltip("Enables looping for waypoints")]
+    public bool loop = true;
+
+    public List<GameObject> waypoints;
+   
+}
