403ebe5f2335f9e5a35a7fc506aa901624d191b6
madelmund <mat.adelmund@gmail.com>
2019-10-12 16:18:38 -0700

    adding switches, and the Ability axis so we can map all abilities to the same button

---
+++ TopDownGameCollab/Assets/Scripts/Flags.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flags : MonoBehaviour
+{
+    [System.Serializable]
+    public enum Ability { None, FlipSwitches, StandOnSwitches, Jump, DestroyObjects };
+    public Ability ability;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TopDownGameCollab/Assets/Scripts/LaserSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSwitch : MonoBehaviour
+{
+    [Tooltip("Drag the laser object from the hierarchy that this switch will destroy")]
+    public GameObject lasers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Flags>() != null)
+        {
+            if (collision.gameObject.GetComponent<Flags>().ability.ToString().Equals("FlipSwitches") && Input.GetAxis("Ability").Equals(1))
+            {
+                lasers.active = false;
+            }
+        }
+    }
+}
