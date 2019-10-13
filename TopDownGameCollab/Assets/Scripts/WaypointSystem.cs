38506cd26b8ff762a8de09e828769c079d4957a3
CyberdineStudios <45576180+CyberdineStudios@users.noreply.github.com>
2019-10-12 17:02:01 -0700

    Waypoint System Minor Update / Sample Scene Update
    Tool Tips have been added to the waypoint system script.  The waypoint sample scene now has walls and a character to follow waypoints.

--- TopDownGameCollab/Assets/Scripts/WaypointSystem.cs
+++ TopDownGameCollab/Assets/Scripts/WaypointSystem.cs
@@ -7,6 +7,9 @@ public class WaypointSystem : MonoBehaviour
     [Tooltip("Enables looping for waypoints")]
     public bool loop = true;
 
+    [Tooltip("Add or remove waypoints by increasing or decreasing the size value")]
     public List<GameObject> waypoints;
+
+
    
 }
